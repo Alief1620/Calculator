5c029fcd9a45a625f2d421acda74649a0d62fcc7
Alief1620 <Alyashakirova16@gmail.com>
2023-08-24 01:59:14 +0200

    Create calculator

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
-number_1 = int(input("Enter a number 1: "))
-number_2 = int(input("Enter a number 2: "))
-operators = input("Enter a operators: ")
-if operators == '+':
-    print(number_1 + number_2)
-elif operators == '-':
-    print(number_1 - number_2)
-elif operators == '*':
-    print(number_1 * number_2)
-elif number_2 == 0:
-    print("is not divided into zero")
-elif operators == '/':
-    print(number_1 / number_2)
-else:
-    print("Bad operator")
+while True:
+        try:
+            number_1 = int(input("Enter a number 1:"))
+            number_2 = int(input("Enter a number 2:"))
+        except ValueError:
+            print("Invalid literal sign")
+            exit()
+
+        operators = input("Enter a operators: ")
+
+        if operators == '+':
+            print(f' Result: {number_1 + number_2}')
+        elif operators == '-':
+            print(f' Result: {number_1 - number_2}')
+        elif operators == '*':
+            print(f' Result: {number_1 * number_2}')
+        elif number_2 == 0:
+            print("is not divided into zero")
+        elif operators == '/':
+            print(f' Result: {number_1 / number_2}')
+        else:
+            print("Bad operator")
+
+
+
+
