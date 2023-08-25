1d151bdb1a87da2861f68e09f082aac788f81555
Alief1620 <Alyashakirova16@gmail.com>
2023-08-25 12:29:28 +0200

    Create calculator

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ while True:
         except ValueError:
             print("Invalid literal sign")
 
-
         operators = input("Enter a operators: ")
 
         if operators == '+':
             print(f' Result: {number_1 + number_2}')
+            user_input = input("Do you want to exit the program? (Yes or not): ")
+            if user_input.lower() == "yes":
+                exit()
         elif operators == '-':
             print(f' Result: {number_1 - number_2}')
         elif operators == '*':
@@ -21,7 +23,6 @@ while True:
         else:
             print("Bad operator")
 exit()
-
 
 
 
