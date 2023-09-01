5da87bbe9e66fbef42a8ef88236a33a086df99c8
Alief1620 <Alyashakirova16@gmail.com>
2023-09-01 14:24:01 +0200

    New version RPN.py

--- RPN.py
+++ RPN.py
@@ -1,46 +1,38 @@
-# Example: 4 5 +
-user_str = input("Enter a numbers and operands with space: ")
-user_str = user_str.split()
-print(user_str)
-def my_function(x, y, operator):
-    if operator == '+':
-        return x + y
-    elif operator == '-':
-        return x - y
-    elif operator == '*':
-        return x * y
-    elif operator == 0:
-        print('is not divided into zero')
-    elif operator == '/':
-        return x / y
+while True:
+    stack = []
+    user_str = input('Enter a numbers and operands with space: ')
+    user_str = user_str.split()
+    for operator in user_str:
+        if operator == '+':
+            x = stack.pop()
+            y = stack.pop()
+            stack.append(x + y)
+        elif operator == '-':
+            x = stack.pop()
+            y = stack.pop()
+            stack.append(x - y)
+        elif operator == '*':
+            x = stack.pop()
+            y = stack.pop()
+            stack.append(x * y)
+        elif operator == '/':
+            x = stack.pop()
+            y = stack.pop()
+            try:
+                y == 0
+            except ValueError:
+                print('Is not divided into zero')
+                print(f'The last of result {x}')
+                stack.append(x / y)
+        else:
+            try:
+                stack.append(int(operator))
+            except ValueError:
+                print('Invalid literal sign')
+    try:
+        print(stack[0])
+    except IndexError:
+        print('Invalid literal sign in list index')
     user_input = input('Do you want to exit the program? (Yes or not): ')
-    if user_input.lower() == 'no':
+    if user_input.lower() == 'yes':
         exit()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
